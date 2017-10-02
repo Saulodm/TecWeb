d870ec60a7e42cecab3feffc42d306fd11bf9c4c
sdmedeiros <sdmedeiros@STFBHZN002510-W>
2017-10-02 12:35:57 -0300

    package.json e porta 3000

--- TecWeb/server.js
+++ TecWeb/server.js
@@ -37,7 +37,7 @@ app.get("/Lista/",function(req,res){
 });
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("app listening at http://%s:%s", host, port)
