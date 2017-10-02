8b5eae754380cf0c18b505cb8e2597061bba1978
sdmedeiros <sdmedeiros@STFBHZN002510-W>
2017-10-02 15:32:59 -0300

    criando rotas

--- TecWeb/app.js
+++ TecWeb/app.js
@@ -1,5 +1,21 @@
 (function() {
  
- angular.module('app', ['ngMap']);
-
+ angular.module('app', ['ui.router','ngMap']);
+ angular
+ .module('app')
+ .config(['$stateProvider', '$urlRouterProvider', routeConfig]);
+ function routeConfig($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider.state('Home', {
+        url: '/',
+        views: {
+            'content': {
+                templateUrl: 'index.html',
+                controller: 'SearchController',
+                controllerAs: 'ct'
+            }
+        }
+    });
+    $urlRouterProvider.otherwise('/');
+}
 })();
